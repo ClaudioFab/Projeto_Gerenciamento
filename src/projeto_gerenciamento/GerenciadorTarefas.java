fcb6ca8a310cd66a7cf7482de8c48d1d43e0de3d
ClaudioFab <cla.rob.fabiano@gmail.com>
2026-05-08 19:27:31 -0300

    first commit

---
+++ src/projeto_gerenciamento/GerenciadorTarefas.java
@@ -0,0 +1,6 @@
+
+package projeto_gerenciamento;
+
+public class GerenciadorTarefas {
+    
+}
---
+++ src/projeto_gerenciamento/Main_Gerenciamento.java
@@ -0,0 +1,10 @@
+
+package projeto_gerenciamento;
+
+public class Main_Gerenciamento {
+
+    public static void main(String[] args) {
+        //inicio main
+    }
+    
+}
